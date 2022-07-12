bfe938a1100b7ab8029959d6ce22a1d4ec776b53
Dmitrii Nechaev <93075740+dmitrii1312@users.noreply.github.com>
2022-07-13 00:06:06 +0300

    Update subnet id

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "yandex_compute_instance" "vm" {
     }
   }
   network_interface {
-    subnet_id = "e9bjuphmhoj2rebvm4ca"
+    subnet_id = "e9br8kho6hlc06mckiem"
   }
   metadata = {
     ssh-keys = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDLCEO+rJTUNexhZNjQlbC64jWUw9b84mP8bknK6gOUvR7m/SmIE/bPv5KBeovy4dmxPyGjBCGJzra0xqN4Q/Q807Lg60hNXEYfa7pmdIvrHLlyfK+FrxHlXSgk402Rikx3FH30utZswIjjsmnZ4XWOwnsl+JyeJknnQ2lMrLmEga6vj3v8AT3r1SOvwhpnTA2vO2iVl7J7tFG/VBDal6GrcKWdld3aQLdHuReAmNu/lewIdK3XDEpPWrmZE89Mcs4VT3MBMo9m4zBxAUzdvs7KEsd2j7oxitmorU9JH+x6T9FYNsfjnlEbqSmuQ8R9hv800dXvlKKe1dZ4KOWocZcpncpqdIkL7dY3YXT2hPco6f61qjuIW3QouJ3xDRyH4inOHlRTlzLuFKsyhjfwzqE68C0RySnKL/z4q+5JMeMPQJquhQj+5tLg+l/GTda6XtBr5SMgVXKARlHf43bpPNPfTys3s2N5SYUrhpVqr7vblGRa9XI+3s7C6xqjZ3b3MqM= dmitrii@dmitrii-virtual-machine"
